563afede2ce3fd288f328ac880d1980a50c40613
unknown <sampv67@gmail.com>
2018-05-15 18:31:55 +0100

    ignore and start of files/folders

---
+++ model/connection.php
@@ -0,0 +1,20 @@
+<?php
+ class Db{
+	 require_once("deets/deets.php");
+	 
+	 private static $instance = null;
+	 
+	 private function __construct(){}
+	 private function __clone(){}
+	 
+	 
+	 public static function getInstance(){
+		 if(!isset(self::$instance)){
+			 
+			 $instance = new PDO('mysql:host=localhost;dbname=mvcrud',$user,$pass);
+		 }
+		 
+		 return self::$instance;
+	 }
+	 
+ }
