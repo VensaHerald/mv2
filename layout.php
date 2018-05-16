98fd0195094ef4d537640f5f30a650eac8c8e653
unknown <sampv67@gmail.com>
2018-05-16 17:11:10 +0100

    started sketching basic layout

---
+++ layout.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<meta></meta>
+<link rel="stylesheet" href="style.css">
+</head>
+<header>
+List of Posts<br />
+
+
+</header>
+
+<body>
+
+<table>
+<th><a href="" class="sort">id</a></th><th><a href="" class="sort">title</a></th><th><a href="" class="sort">content</a></th><th><a href="" class="sort">options</a></th>
+<tr>
+<td>123</td><td>this post</td><td>the detail of this post</td><td> delete-edit</td>
+</tr>
+<tr>
+<td>124</td><td>this post</td><td>the detail of this post</td><td> delete-edit</td>
+</tr>
+<tr>
+<td>125</td><td>this post</td><td>the detail of this post</td><td> delete-edit</td>
+</tr>
+
+</table>
+
+
+</body>
+
+
+</html>
